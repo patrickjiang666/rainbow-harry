4190ed173c313f046e3ca8eb6d0a2622781a2c72
uken.harryw <uken.harryw@gmail.com>
2022-02-21 21:02:22 -0500

    layout

---
+++ components/footer/index.js
@@ -0,0 +1,4 @@
+export default function footer ( ) {
+    return <div>hello footer</div>
+
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
+import Layout from '../components/layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Layout> 
+    <Component {...pageProps} />
+  </Layout>
 }
 
 export default MyApp
+
+//self deconstruction
+//let a = [1,2,3]
+//let b = [...a]
+// let a = {
+//   a: '1',
+//   b: '1',
+//   c: '1',
+// }
+// let b = {...a}
