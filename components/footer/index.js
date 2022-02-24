c4564a769bd33d975f48377a9d4ba8e7a0c17c85
uken.harryw <uken.harryw@gmail.com>
2022-02-23 19:13:42 -0500

    footer

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,3 +1,18 @@
+import {BsGrid3X3GapFill} from 'react-icons/bs'
+
 export default function footer ( ) {
-    return <div className="footerWrapper">hello footer</div>
+    return <div className="footerWrapper">
+        <div className="footerInfo"> 
+            Rainbow Glof Club, 3822 County Rte 26, Greenville, NY 12083
+        </div>
+        <div className="footerInfo"> 
+            islandgreenwebsite@gamil.com
+        </div>
+        <div className="footerInfo"> 
+            (518)966-5343
+        </div>
+        <div className="footerInfo"> 
+            Copyright 2022. rainbowgolf.co All Rights Reserved.
+        </div>
+    </div>
 }
