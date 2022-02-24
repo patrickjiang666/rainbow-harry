086179a33529f3d14761b8fc6f0ab066e26dca2d
uken.harryw <uken.harryw@gmail.com>
2022-02-23 19:13:26 -0500

    footer

--- components/navBar/index.js
+++ components/navBar/index.js
@@ -1,13 +1,21 @@
 export default function navBar ( ) {
     return <div className="headerWrapper">
         <div className="navBar">
-            <span> RAINBOW GOLD CLUB </span>
-            <menu>
-                <button a="">HOME</button>
-                <button a="">GOLF</button>
-                <button a="">LODGE</button>
-                <button a="">CONTACT</button>
-        </menu>
+            <div className="childDiv"> 
+                <span> RAINBOW GOLD CLUB </span>
+            </div>
+            <div className="childDiv"> 
+                <menu>
+                    <button a="">HOME</button>
+                    <button a="">GOLF</button>
+                    <button a="">LODGE</button>
+                    <button a="">CONTACT</button>
+                </menu>
+            </div>
+            <div className="childDiv"> </div>
+            <div className="childDiv"> </div>
+            
+            
         </div>
         
         
